b7326727b28c57792991401549b0e56755654d26
Jay Do <jay@cal-se.com>
2023-06-04 22:31:38 +0700

    build: :rocket: Stumble MenuOn.site

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/stumble',
+            destination: 'https://oddmenu.com/p/the-stumble',
+            permanent: true,
+          },
+        ];
+      },
+}
 
 module.exports = nextConfig
